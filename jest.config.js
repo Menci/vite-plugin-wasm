e501eb234ad22d81d9bc94cb20086b759b4b07df
Menci <huanghaorui301@gmail.com>
2022-03-04 17:09:28 +0800

    test: exclude built files from jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["jest-extended/all"]
+  setupFilesAfterEnv: ["jest-extended/all"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"]
 };
