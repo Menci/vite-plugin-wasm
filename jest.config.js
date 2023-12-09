5e2353640fe1da2bbd77a30a0035bad8e781cef6
Menci <huanghaorui301@gmail.com>
2023-12-10 02:42:27 +0800

    Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest/presets/default-esm",
-  globals: {
-    "ts-jest": {
-      useESM: true
-    }
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest', {
+        useESM: true
+      }
+    ]
   },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1"
